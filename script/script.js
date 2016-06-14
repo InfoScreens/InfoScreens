b965ddd2cd0f0e35343ff2932d0f165a6e011e97
Alex Kott <alexey.kott@gmail.com>
2016-06-14 08:50:39 +0300

    переношу на сервер

--- script/script.js
+++ script/script.js
@@ -1,50 +1,41 @@
 //------timeline script
 
+var container = document.getElementById("timeline");
 
-var bodyBlock = $('#timeline');
-var slider = $('#inner_wrap');
-var rails = $('#outer_wrap');
-var startPos = curPos = 0;
-var cW = slider.width();
-var bW = bodyBlock.width();                          
-var sideSpace = cW - bW;
-rails.css({
-    width: parseInt(cW + sideSpace,10),
-    left:  -sideSpace,
-    top: 5
-});
 
-function softStop(dest){
-    slider.animate({left: dest},750, function(){
-        if(dest < 0){
-            softStop(0);
-        }
-        if(dest > sideSpace){
-            softStop(sideSpace);
-        }
-    });
-    return false;
-}
+  // Create a DataSet (allows two way data-binding)
+  var items = new vis.DataSet([
+    {id: 1, content: 'A', start: '2015-02-09T04:00:00'},
+    {id: 2, content: 'B', start: '2015-02-09T14:00:00'},
+    {id: 3, content: 'C', start: '2015-02-09T16:00:00'},
+    {id: 4, content: 'D', start: '2015-02-09T17:00:00'},
+    {id: 5, content: 'E', start: '2015-02-10T03:00:00'}
+  ]);
 
-slider.draggable({ 
-    containment:rails, 
-    axis: 'x',
-    cursor: 'pointer',
-    drag: function(e,ui){
-        curPos = ui.position.left;
-    },
-    stop: function(e,ui){
-        if(curPos > startPos && curPos > 0){
-            startPos = curPos + 150;
-        } else {
-            startPos = curPos - 150;
-        }
-        softStop(startPos);
+  // Configuration for the Timeline
+  var options = {
+    editable: true,
+
+    // always snap to full hours, independent of the scale
+    snap: function (date, scale, step) {
+      var hour = 60 * 60 * 1000;
+      return Math.round(date / hour) * hour;
     }
-});
+
+    // to configure no snapping at all:
+    //
+    //   snap: null
+    //
+    // or let the snap function return the date unchanged:
+    //
+    //   snap: function (date, scale, step) {
+    //     return date;
+    //   }
+  };
+
+  // Create a Timeline
+  var timeline = new vis.Timeline(container, items, options);
 
 
 
-
-
-//------end of timeline script
+//------end of timeline 
