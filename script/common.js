132b3c9fb85d5085ae526fc6f42277b9ae7701d0
turuslan <turuslan.devbox@gmail.com>
2016-08-09 12:32:46 +0300

    `display_error`, `action_callback_display_error`;

--- script/common.js
+++ script/common.js
@@ -15,3 +15,22 @@ function check_user_authorized (require_admin) {
     );
 }
 
+function display_error (error) {
+    notif ({
+        msg: get_error_text (error),
+        type: "error",
+        position: "center",
+        timeout: 3000,
+        multiline: true
+    });
+}
+
+function action_callback_display_error (success) {
+    return function (response) {
+        if (response.errored ()) {
+            display_error (response.error);
+        } else {
+            success.call (window, response);
+        }
+    };
+}
