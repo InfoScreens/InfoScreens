d49377a6368813659cfd7718c81dd48c98907c95
turuslan <turuslan.devbox@gmail.com>
2016-08-20 10:37:39 +0300

    state.js, fix state.php path;

--- script/state.js
+++ script/state.js
@@ -1,7 +1,7 @@
 
 function State (device_id, app_id) {
 	this.set = function (state) {
-		$.post ("state.php?method=set_state&data=" + window.escape (JSON.stringify ({
+		$.post ("/state.php?method=set_state&data=" + window.escape (JSON.stringify ({
 			device_id: device_id,
 			app_id: app_id,
 			state: state
